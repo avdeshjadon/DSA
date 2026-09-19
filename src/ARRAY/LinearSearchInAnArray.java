59fcba7c2cde2a32b757cdd22fda6147d23f3dee
avdeshjadon <theavdeshjadon@gmail.com>
2026-09-19 07:48:00 +0530

    commit

---
+++ src/ARRAY/LinearSearchInAnArray.java
@@ -0,0 +1,27 @@
+package ARRAY;
+
+public class LinearSearchInAnArray {
+
+    public static int search(int[] arr, int num) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == num) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 3, 5, 4, 56, 332, 56, 45, 566, 356, 3};
+
+        int num = 4;
+
+        int result = search(arr, num);
+
+        System.out.println(result);
+    }
+}
